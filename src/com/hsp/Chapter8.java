f06030644dbd15d2f7477cde9715bef19affd6d6
xsy_9211 <xsy_9211@126.com>
2024-04-21 14:08:04 +0000

    UPDATE CHAPTER 8

--- src/com/hsp/Chapter8.java
+++ src/com/hsp/Chapter8.java
@@ -8,12 +8,14 @@ package com.hsp;
 public class Chapter8 {
     void testP279(){
         Vtuber asoul = new Vtuber("A-SOUL",12_000,true,teamTypes.ENTERPRISE);
+        asoul.counts++;
         asoul.get();
         asoul.addSalary(6000);
         asoul.get();
         Profession t = new Vtuber("ava", 0, true, teamTypes.SOLO);
         //t.dance();// error 向上转型 父类的引用指向子类的对象 
                     //可以使用父类的所有成员（遵守访问权限） 不能使用子类特有的成员（编译阶段决定的）
+        t.counts++;
         System.out.println(t instanceof Vtuber);//true
         System.out.println(t instanceof Profession);//true
 
@@ -21,6 +23,7 @@ public class Chapter8 {
                                 //1.只能转换引用，不能转换对象
                                 //2.转换的目标引用要指向目前所声明的编译类型对象
                                 //3.向下转型后 可以调用子类所有的成员
+        b.counts++;
         System.out.println(b.equals(t));// true
         System.out.println(b.equals(asoul));//false
         System.out.println(b instanceof Vtuber);//true
@@ -30,7 +33,9 @@ public class Chapter8 {
         System.out.println("asoul:"+asoul.getClass());
         System.out.println("t:"+t.getClass()); // getClass() return running Class
         System.out.println("b:"+b.getClass());
-        
+        System.out.println("asoul:"+asoul.counts);
+        System.out.println(asoul.counts==b.counts?asoul.counts==t.counts?true:false:false);
+
 
     }
 }
@@ -38,6 +43,7 @@ class Profession{
     String name;
     double meanSalary;
     boolean isNew;
+    public static int counts = 0;
     public Profession(String name,double meanSalary,boolean isNew){
         this.isNew = isNew;
         this.name = name;
@@ -54,7 +60,7 @@ class Profession{
         if(this == t){return true;}
         if(t instanceof Profession){
             Profession p =(Profession) t;
-        if(this.isNew == p.isNew&&this.name.equalsIgnoreCase(p.name)&&(this.meanSalary-p.meanSalary<0.00001)){
+        if(this.isNew == p.isNew&&this.name.equalsIgnoreCase(p.name)&&(Math.abs(this.meanSalary-p.meanSalary)<0.00001)){
             return true;
         }
     }
@@ -91,7 +97,6 @@ class Vtuber extends Profession{
         System.out.println("Vtuber" +this.name +" almost can dance.");
 
     }
-
 
 
 }
